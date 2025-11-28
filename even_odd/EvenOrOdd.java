adc6708d82b148a790ab12019e978feb2ea2d1d5
Riya-gupta1819 <riyagupta4721@gmail.com>
2025-11-28 20:32:01 +0530

    Add files via upload

---
+++ even_odd/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package com.riya.even_odd;
+
+public class EvenOrOdd {
+
+	 public static boolean isEven(int n) {
+        if (n % 2 == 0) {
+            return true;   // even
+        } else {
+            return false;  // odd
+        }
+    }
+
+}
---
+++ even_odd/Test.java
@@ -0,0 +1,16 @@
+package com.riya.even_odd;
+
+public class Test {
+
+	public static void main(String[] args) {
+		 int n = 16;
+	        boolean s = EvenOrOdd.isEven(n);  // pass n as argument
+
+	        if (s) {
+	            System.out.println(n + " is even");
+	        } else {
+	            System.out.println(n + " is odd");
+	        }
+	}
+
+}
