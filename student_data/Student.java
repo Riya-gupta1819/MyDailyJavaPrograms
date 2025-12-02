d958a269382bb0cc9fce94f0ed7bae20b91c77c0
Riya-gupta1819 <riyagupta4721@gmail.com>
2025-12-02 23:04:31 +0530

    Add files via upload

---
+++ student_data/Student.java
@@ -0,0 +1,8 @@
+package com.riya.student_data;
+
+public class Student {
+  public static String getStudentDetails(int roll, String name, double fee)
+  {
+	 return "[Roll is: "+roll+", Name is: "+name+", Fee is: "+fee+"]";
+  }
+}
---
+++ student_data/Test.java
@@ -0,0 +1,12 @@
+package com.riya.student_data;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String s = Student.getStudentDetails(1,"ravi" ,500);
+		System.out.println(s);
+
+	}
+
+}
