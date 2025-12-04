c1f8d4ce7a80f8a5b7387a0c048a5b1be1e904c8
Riya Gupta <riyagupta4721@gmail.com>
2025-12-04 22:45:31 +0530

    Add files via upload

---
+++ sum_of_digits/Test.java
@@ -0,0 +1,16 @@
+package com.riya.sum_of_digits;
+
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		int num = 34;
+		int sum = TwoDigitsSum. getSumOfDigitsSum(num);
+		
+		System.out.println("Sum of two number is: "+sum);
+		
+
+	}
+
+}
---
+++ sum_of_digits/TwoDigitsSum.java
@@ -0,0 +1,11 @@
+package com.riya.sum_of_digits;
+
+public class TwoDigitsSum {
+	public static int getSumOfDigitsSum(int num)
+	{
+		int a = num/10;
+		int b = num%10;
+		return a+b;
+	}
+
+}
