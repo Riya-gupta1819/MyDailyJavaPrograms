992c84167176b5ddfcfc154a90e40ff2efecbb91
Riya Gupta <riyagupta4721@gmail.com>
2025-12-26 21:10:25 +0530

    Add files via upload

---
+++ no_repeat/NoRepeatCharacter.java
@@ -0,0 +1,17 @@
+package com.riya.no_repeat;
+
+public class NoRepeatCharacter {
+
+	public static void main(String[] args) {
+		String s = "swiss";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))) {
+                System.out.println(s.charAt(i));
+                break;
+            }
+        }
+
+	}
+
+}
