7ae60a7afd204dda0ff339c546a491ab0c9dbe5b
Riya Gupta <riyagupta4721@gmail.com>
2026-01-02 21:34:08 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,23 @@
+package com.riya.pattern;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int space = 1; space <= n - i; space++) {
+                System.out.print(" ");
+            }
+
+            for (int star = 1; star <= i; star++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+	}
+
+}
