84ec2a53990d9d6f17f2274fc3c3fd827c8b66c8
Riya Gupta <riyagupta4721@gmail.com>
2026-01-10 22:29:42 +0530

    Add files via upload

---
+++ Pattern12.java
@@ -0,0 +1,21 @@
+package com.riya.pattern;
+
+public class Pattern12 {
+
+	public static void main(String[] args) {
+		int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s <= n - i; s++)
+                System.out.print(" ");
+
+            for (int j = 1; j <= i; j++)
+                System.out.print(j + " ");
+
+            System.out.println();
+        }
+
+	}
+
+}
