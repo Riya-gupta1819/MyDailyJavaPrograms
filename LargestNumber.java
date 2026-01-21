e838d2a6bf380571b14d8ef333a52b03d3a03be0
Riya Gupta <riyagupta4721@gmail.com>
2026-01-21 21:02:21 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,20 @@
+package com.riya.reverse;
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		int[] arr = {12, 45, 7, 89, 23};
+
+        int max = arr[0];   // assume first element is largest
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Largest number = " + max);
+
+	}
+
+}
