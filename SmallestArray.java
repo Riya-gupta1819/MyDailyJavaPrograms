f3ac5ac0a095b613e6f1e1748ae5589b9297e377
Riya Gupta <riyagupta4721@gmail.com>
2026-01-22 21:38:34 +0530

    Add files via upload

---
+++ SmallestArray.java
@@ -0,0 +1,20 @@
+package com.riya.reverse;
+
+public class SmallestArray {
+
+	public static void main(String[] args) {
+		int[] arr = {12, 45, 7, 89, 23};
+
+        int min = arr[0];   // assume first element is smallest
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Smallest number = " + min);
+
+	}
+
+}
