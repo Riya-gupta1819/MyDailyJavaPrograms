a000858f16c0d0862e26aadd3d125128962e2265
Riya Gupta <riyagupta4721@gmail.com>
2026-01-26 21:30:37 +0530

    Add files via upload

---
+++ MoveAllZerostoEnd.java
@@ -0,0 +1,22 @@
+package com.riya.reverse;
+
+public class MoveAllZerostoEnd {
+
+	public static void main(String[] args) {
+		 int[] arr = {0, 1, 0, 3, 12};
+	        int index = 0;
+
+	        for (int i = 0; i < arr.length; i++) {
+	            if (arr[i] != 0)
+	                arr[index++] = arr[i];
+	        }
+
+	        while (index < arr.length)
+	            arr[index++] = 0;
+
+	        for (int num : arr)
+	            System.out.print(num + " ");
+
+	}
+
+}
