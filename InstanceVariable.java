b43e2d3429471fcc3b5b64ba1b065f9f4cda3630
Riya Gupta <riyagupta4721@gmail.com>
2026-02-05 22:23:20 +0530

    Add files via upload

---
+++ InstanceVariable.java
@@ -0,0 +1,18 @@
+package com.riya.instance.variable;
+
+public class InstanceVariable {
+	//instance variable
+	int students ;
+	
+	
+	
+
+	public static void main(String[] args) {
+		InstanceVariable  obj = new InstanceVariable();
+		System.out.println(obj.students);
+		
+		
+
+	}
+
+}
