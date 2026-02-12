c95e33cfb9e0479d5697328325b64f1c8284e8f3
Riya Gupta <riyagupta4721@gmail.com>
2026-02-12 22:10:27 +0530

    Add files via upload

---
+++ AutoBoxing.java
@@ -0,0 +1,13 @@
+package com.riya.autoboxing;
+
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		  int a = 10;          // primitive
+	        Integer obj = a;    // autoboxing (int -> Integer)
+
+	        System.out.println("Primitive value: " + a);
+	        System.out.println("Wrapper object: " + obj);
+	}
+
+}
