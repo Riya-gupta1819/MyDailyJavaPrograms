ea96aa7256981389817853a258b363f2aa167782
Riya Gupta <riyagupta4721@gmail.com>
2026-02-13 21:29:37 +0530

    Add files via upload

---
+++ UnBoxing.java
@@ -0,0 +1,13 @@
+package com.riya.autoboxing;
+
+public class UnBoxing {
+
+	public static void main(String[] args) {
+		 Integer obj = 20;   // wrapper object
+	        int a = obj;        // unboxing (Integer -> int)
+
+	        System.out.println("Wrapper object: " + obj);
+	        System.out.println("Primitive value: " + a);
+	}
+
+}
