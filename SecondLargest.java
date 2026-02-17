d345dcc68294bc8007f61942edd959944fa64c54
Riya Gupta <riyagupta4721@gmail.com>
2026-02-17 21:53:40 +0530

    Add files via upload

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,23 +1,28 @@
-package com.riya.reverse;
+package com.riya.practice;
 
 public class SecondLargest {
 
 	public static void main(String[] args) {
-		int[] arr = {10, 5, 20, 8, 15};
-
-        int max = Integer.MIN_VALUE;
-        int second = Integer.MIN_VALUE;
-
-        for (int num : arr) {
-            if (num > max) {
-                second = max;
-                max = num;
-            } else if (num > second && num != max) {
-                second = num;
-            }
-        }
-
-        System.out.println("Second Largest = " + second);
+		int[] arr = {12, 35, 1, 10, 34, 1};
+		Integer first = null, second = null;
+		
+		for(int n : arr) {
+			if(first == null || n > first) {
+				second = first;
+				first = n;
+			}
+			else if((second == null || n>second) && n != first)
+			{
+				second = n;
+			}
+		}
+		
+		if(second != null) {
+			System.out.println("Second largest: "+second);
+		}
+		else {
+			System.out.println("No distinct second largest value.");
+		}
 
 	}
 
