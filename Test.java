634047a8ba41464b65a03305a0af83480e3effba
Riya Gupta <riyagupta4721@gmail.com>
2026-02-23 22:32:53 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,25 +1,25 @@
-package com.riya.staticc;
+package riya.com.basics_programs;
+
+class Parent {
+
+    Parent() {
+        System.out.println("Parent Constructor Called");
+    }
+}
+
+class Child extends Parent {
+
+    Child() {
+        super();   // Calls Parent constructor
+        System.out.println("Child Constructor Called");
+    }
+}
 
 public class Test {
 	
-	int x = 0;
-	static int y = 1;
-	
-	void display() {
-		System.out.println("Static "+y);
-		System.out.println("Instant "+x);
-	}
 
 	public static void main(String[] args) {
-		Test d1 = new Test();
-		d1.display();
-		
-		Test d2 = new Test();
-		d2.display();
-		
-		Test d3 = new Test();
-		d3.display();
-
+		Child obj = new Child();
 	}
 
 }
