552cba69f39758ad580e4fd47e26b864474ef7b9
Riya Gupta <riyagupta4721@gmail.com>
2026-03-03 22:27:18 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,40 +1,22 @@
-package com.riya.cons.basic;
+package com.riya.reverse;
 
 public class Student {
-	
-	int rollNo;
-    String name;
-    double marks;
+	 String name;   // Instance variable
 
-    // Default constructor
-    Student() {
-        this(101);   // calling parameterized constructor
-        System.out.println("Default Constructor Called");
-    }
+	    // Constructor with same parameter name
+	    Student(String name) {
+	        this.name = name;   // 'this.name' refers to instance variable
+	    }
 
-    // Constructor with one parameter
-    Student(int rollNo) {
-        this(rollNo, "Riya");  // calling another constructor
-        System.out.println("One-Parameter Constructor Called");
-    }
+	    void display() {
+	        System.out.println("Student Name: " + name);
+	    }
 
-    // Constructor with two parameters
-    Student(int rollNo, String name) {
-        this.rollNo = rollNo;
-        this.name = name;
-        this.marks = 85.5;
-        System.out.println("Two-Parameter Constructor Called");
-    }
-
-    void display() {
-        System.out.println("Roll No: " + rollNo);
-        System.out.println("Name: " + name);
-        System.out.println("Marks: " + marks);
-    }
 
 	public static void main(String[] args) {
-		  Student s = new Student();
-	        s.display();
+		Student s1 = new Student("Riya");
+        s1.display();
+
 	}
 
 }
