5ef7dafc6fe126b499a7acad6b176fd1cf355859
Riya Gupta <riyagupta4721@gmail.com>
2026-03-07 23:03:44 +0530

    Add files via upload

---
+++ Matrix1.java
@@ -0,0 +1,23 @@
+package com.riya.reverse;
+
+public class Matrix1 {
+
+	public static void main(String[] args) {
+		int[][] matrix = {
+	            {1, 2, 3},
+	            {4, 5, 6}
+	        };
+
+	        int sum = 0;
+
+	        for(int i = 0; i < matrix.length; i++) {
+	            for(int j = 0; j < matrix[i].length; j++) {
+	                sum = sum + matrix[i][j];
+	            }
+	        }
+
+	        System.out.println("Sum = " + sum);
+
+	}
+
+}
