afff672ae912568d7ee2e0ca6a54e13aeb54ce9c
Riya Gupta <riyagupta4721@gmail.com>
2026-03-25 21:57:40 +0530

    Add files via upload

--- Test6.java
+++ Test6.java
@@ -1,39 +1,23 @@
-package com.riya.reverse;
+package com.riya.practice;
+
+class Student1 {
+
+    void study() {
+        System.out.println("Student is studying");
+    }
+
+    void play() {
+        System.out.println("Student is playing");
+    }
+}
 
 public class Test6 {
 
 	public static void main(String[] args) {
-		 int[] arr1 = {1, 2, 4, 5};
-	        int[] arr2 = {2, 3, 5, 6};
+		Student1 s1 = new Student1();   // object creation using new
 
-	        int i = 0, j = 0;
-
-	        while(i < arr1.length && j < arr2.length) {
-
-	            if(arr1[i] < arr2[j]) {
-	                System.out.print(arr1[i] + " ");
-	                i++;
-	            }
-	            else if(arr1[i] > arr2[j]) {
-	                System.out.print(arr2[j] + " ");
-	                j++;
-	            }
-	            else {
-	                System.out.print(arr1[i] + " ");
-	                i++;
-	                j++;
-	            }
-	        }
-
-	        while(i < arr1.length) {
-	            System.out.print(arr1[i] + " ");
-	            i++;
-	        }
-
-	        while(j < arr2.length) {
-	            System.out.print(arr2[j] + " ");
-	            j++;
-	        }
+        s1.study();   // method call
+        s1.play();    // method call
 
 	}
 
