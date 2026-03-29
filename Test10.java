6f51b03ea24dba50a0424bc8ac20be6403076b4c
Riya Gupta <riyagupta4721@gmail.com>
2026-03-29 22:05:46 +0530

    Add files via upload

--- Test10.java
+++ Test10.java
@@ -1,14 +1,50 @@
-package com.riya.reverse;
+package com.riya.practice;
 
+class Shape {
+
+    void draw() {
+        System.out.println("Drawing Shape");
+    }
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+class ShapeFactory {
+
+    static Shape getShape(String type) {
+
+        if (type.equalsIgnoreCase("circle")) {
+            return new Circle();
+        }
+        else if (type.equalsIgnoreCase("rectangle")) {
+            return new Rectangle();
+        }
+        else {
+            return null;   // no matching type
+        }
+    }
+}
 public class Test10 {
 
 	public static void main(String[] args) {
-		String s1 = "Java";
-		String s2 = new String("Java");
-		
-		System.out.println(s1=s2);
-		s2=s2.intern();
-		System.out.println(s1==s2);
+		Shape s1 = ShapeFactory.getShape("circle");
+        if (s1 != null)
+            s1.draw();
+
+        Shape s2 = ShapeFactory.getShape("triangle");
+        if (s2 != null)
+            s2.draw();
+        else
+            System.out.println("No matching shape found");
 
 	}
 
