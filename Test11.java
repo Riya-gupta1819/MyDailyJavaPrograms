50ab033ea962b03a2fa8f7a8bf2427e908d4c623
Riya Gupta <riyagupta4721@gmail.com>
2026-03-30 22:44:05 +0530

    Add files via upload

--- Test11.java
+++ Test11.java
@@ -1,34 +1,23 @@
-package com.riya.reverse;
-
-import java.util.Scanner;
+package com.riya.practice;
 
 public class Test11 {
+	
+	static String concat(String... words) {
 
-	public static void main(String[] args) {
+        String result = "";
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a sentence: ");
-        String str = sc.nextLine();
-
-        String[] words = str.split(" ");
-
-        String smallest = words[0];
-        String largest = words[0];
-
-        for(int i = 1; i < words.length; i++) {
-
-            if(words[i].length() < smallest.length()) {
-                smallest = words[i];
-            }
-
-            if(words[i].length() > largest.length()) {
-                largest = words[i];
-            }
+        for (String word : words) {
+            result = result + word;
         }
 
-        System.out.println("Smallest word = " + smallest);
-        System.out.println("Largest word = " + largest);
+        return result;
+    }
+
+	public static void main(String[] args) {
+		 String res = concat("Java ", "is ", "easy");
+
+	        System.out.println(res);
+
 	}
 
 }
