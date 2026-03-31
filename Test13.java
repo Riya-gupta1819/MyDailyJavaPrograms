6145ca40133ee1b1bf1314672640c3b3e2497fac
Riya Gupta <riyagupta4721@gmail.com>
2026-03-31 21:05:07 +0530

    Add files via upload

---
+++ Test13.java
@@ -0,0 +1,17 @@
+package com.riya.practice;
+
+public class Test13 {
+	
+	static double average(int a, int b) {
+        return (a + b) / 2.0;
+    }
+
+	public static void main(String[] args) {
+		double result = average(10, 20);
+
+        System.out.println("Average = " + result);
+
+
+	}
+
+}
