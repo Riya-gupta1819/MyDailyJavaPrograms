959261e54c589d6c2b6a5a5b06a20d92bcac9357
Riya Gupta <riyagupta4721@gmail.com>
2026-04-01 22:22:50 +0530

    Add files via upload

---
+++ Test14.java
@@ -0,0 +1,16 @@
+package com.riya.practice;
+
+public class Test14 {
+	
+	static int sum(int a, int b) {
+        return a + b;
+    }
+
+	public static void main(String[] args) {
+		int result = sum(10, 20);
+
+        System.out.println("Sum = " + result);
+
+	}
+
+}
