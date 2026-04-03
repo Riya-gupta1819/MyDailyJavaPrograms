4ef91e67d78904d35a36e538f49c9641d295e730
Riya Gupta <riyagupta4721@gmail.com>
2026-04-03 23:23:30 +0530

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -1,24 +1,9 @@
-package com.riya.practice;
-
-class Demo {
-
-    static {
-        System.out.println("Static block executed");
-    }
-
-    Demo() {
-        System.out.println("Constructor executed");
-    }
-}
+package com.riya.perfect;
 
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Main method started");
-
-        Demo obj = new Demo();
-
-        System.out.println("Main method ended");
+		System.out.println("Riya");
 
 	}
 
