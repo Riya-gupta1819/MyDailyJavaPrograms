2f0d535a54884e692a71514afb02625429af126a
Riya Gupta <riyagupta4721@gmail.com>
2026-04-10 16:28:31 +0530

    Add files via upload

--- Test8.java
+++ Test8.java
@@ -1,27 +1,9 @@
-package com.riya.practice;
-
-class Studentt {
-
-    int id = 101;
-    String name = "Riya";
-
-    void display() {
-        System.out.println("ID: " + id);
-        System.out.println("Name: " + name);
-    }
-}
+package com.riya.perfect;
 
 public class Test8 {
 
 	public static void main(String[] args) {
-		Studentt s1 = new Studentt();   // object creation
-
-        // accessing variables using dot operator
-        System.out.println("ID: " + s1.id);
-        System.out.println("Name: " + s1.name);
-
-        // calling method using dot operator
-        s1.display();
+		// TODO Auto-generated method stub
 
 	}
 
